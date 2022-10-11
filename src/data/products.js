e2d0b922c60ec0df4c9e2dfad67c6782959cdf40
Cong Dinh <dinhdc1111@gmail.com>
2022-10-12 02:14:44 +0700

    feat : update

--- src/data/products.js
+++ src/data/products.js
@@ -4,7 +4,7 @@ const ListProduct = [
         name: "Big and Juicy Wagyu Beef Cheeseburger",
         price: 30,
         desc: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
-        image: "./assets/images/image 29.png",
+        image: "./assets/images/image 26.png",
         category: 1,
     },
     {
@@ -12,7 +12,7 @@ const ListProduct = [
         name: "Fresh Lime Roasted Salmon",
         price: 10,
         desc: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
-        image: "./assets/images/image 30.png",
+        image: "./assets/images/image 27.png",
         category: 1,
     },
     {
@@ -20,7 +20,7 @@ const ListProduct = [
         name: "The Best Easy One Pot Chicken and Rice",
         price: 20,
         desc: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
-        image: "./assets/images/image 31.png",
+        image: "./assets/images/image 28.png",
         category: 3,
     },
     {
--- src/pages/products.js
+++ src/pages/products.js
@@ -27,7 +27,7 @@ const ProductPage = {
                             <a href="/products/${product.id}"><img class="related_products__image-list-PageProducts" src="${product.image}" alt="Image Products"></a>
                             <a href="/products/${product.id}"><h5 class="related_products__title-PageProducts">${product.name}</h5></a>
                             <a href="/products/${product.id}"><h3 class="related_products-price">$${product.price}</h3></a>
-                            <button class="related_products-btnAdd-PageProducts">Add To Cart</button>
+                            <a href="/products/${product.id}"><button class="related_products-btnAdd-PageProducts">Add To Cart</button></a>
                         </div>`).join("")}
                 </div>
             </section>
