2038e9cb6926f90375489adbc672905fb67d98d6
Cong Dinh <dinhdc1111@gmail.com>
2022-10-19 05:15:18 +0700

    form add UI

--- src/pages/admin/productManagement/add.js
+++ src/pages/admin/productManagement/add.js
@@ -6,23 +6,57 @@ const AddNewProduct = {
     render() {
         return /* html */ `
         ${NavAdmin.render()}
-        <section>
-        <form class="mx-auto mt-5" style= "width:500px" id="add-new">
-            <div class="form-outline mb-4">
-                <input type="text" id="title" class="form-control" placeholder="Food name"/>
+        <section class="dashboard">
+        <div class="top">
+            <i class="fas fa-bars sidebar-toggle"></i>
+            <div class="search-box">
+                <i class="fas fa-search"></i>
+                <input type="text" placeholder="Search food here..." >
             </div>
-            <div class="form-outline mb-4">
-                <input type="number" id="price" class="form-control" placeholder="Food price"/>
+        </div>
+
+        <div class="dash-content">
+            <div class="overview">
+                <div class="title">
+                    <span class="text">ADD NEW FOOD</span>
+                </div>
+                <form id="add-new">
+                        <div class="input-control">
+                            <input type="text" id="title" name="food_name" placeholder="Tên món ăn">
+                            <div class="error"></div>
+                            <span class="icon-success"><i class="fas fa-check-circle"></i></span>
+                            <span class="icon-warnning"><i class="fas fa-exclamation-triangle rung"></i></span>
+                        </div>
+                        <div class="input-control">
+                            <input type="number" id="quantity" name="quantity" placeholder="Số lượng món ăn">
+                            <div class="error"></div>
+                            <span class="icon-success"><i class="fas fa-check-circle"></i></span>
+                            <span class="icon-warnning"><i class="fas fa-exclamation-triangle rung"></i></span>
+                        </div>
+                        <div class="input-control">
+                            <input type="number" id="price" name="price" placeholder="Giá món ăn">
+                            <div class="error"></div>
+                            <span class="icon-success"><i class="fas fa-check-circle"></i></span>
+                            <span class="icon-warnning"><i class="fas fa-exclamation-triangle rung"></i></span>
+                        </div>
+                        <div class="input-control">
+                        <div class="mb-3">
+                            <input class="form-control" type="file" id="formFile">
+                        </div>
+                            
+                            <div class="error"></div>
+                        </div>
+                        <div class="input-control">
+                            <textarea rows="5" placeholder="Mô tả món ăn" id="content"></textarea>
+                            <div class="error"></div>
+                            <span class="icon-success"><i class="fas fa-check-circle"></i></span>
+                            <span class="icon-warnning"><i class="fas fa-exclamation-triangle rung"></i></span>
+                        </div>
+                    <button id="add" type="submit">Thêm mới</button>
+                </form>
             </div>
-            <div class="mb-3">
-                <input class="form-control" type="file" id="formFile">
-            </div>
-            <div class="form-outline mb-4">
-                <textarea class="form-control" id="content" rows="4" placeholder="Description"></textarea>
-            </div>
-            <button class="btn btn-primary btn-block mb-4">Send</button>
-        </form>
-        </section>
+        </div>
+    </section>
         `;
     },
     afterRender() {
@@ -42,6 +76,7 @@ const AddNewProduct = {
             });
             const newProduct = {
                 title: document.querySelector("#title").value,
+                quantity: document.querySelector("#quantity").value,
                 price: document.querySelector("#price").value,
                 image: data.url,
                 content: document.querySelector("#content").value,
