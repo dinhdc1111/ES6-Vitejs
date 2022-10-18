86d8268823b69763a2663bb0edee695fd466d52c
Cong Dinh <dinhdc1111@gmail.com>
2022-10-19 04:29:21 +0700

    upload image

--- src/pages/admin/productManagement/add.js
+++ src/pages/admin/productManagement/add.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import NavAdmin from "../../../components/NavAdmin";
+import { add } from "../../../api/products";
 
 const AddNewProduct = {
     render() {
@@ -9,8 +11,8 @@ const AddNewProduct = {
             <div class="form-outline mb-4">
                 <input type="text" id="title" class="form-control" placeholder="Title name"/>
             </div>
-            <div class="form-outline mb-4">
-                <input type="text" id="image" class="form-control" placeholder="Image"/>
+            <div class="mb-3">
+                <input class="form-control" type="file" id="formFile">
             </div>
             <div class="form-outline mb-4">
                 <textarea class="form-control" id="content" rows="4" placeholder="Description"></textarea>
@@ -21,11 +23,23 @@ const AddNewProduct = {
         `;
     },
     afterRender() {
-        document.querySelector("#add-new").addEventListener("submit", (e) => {
+        const CLOUDINARY_PRESET = "sqyr2tx5";
+        const CLOUDINARY_API_URL = "https://api.cloudinary.com/v1_1/dhxziinvx/image/upload";
+        document.querySelector("#add-new").addEventListener("submit", async (e) => {
             e.preventDefault();
+            const fileImage = document.querySelector("#formFile").files[0];
+            const formData = new FormData();
+            formData.append("file", fileImage);
+            formData.append("upload_preset", CLOUDINARY_PRESET);
+
+            const { data } = await axios.post(CLOUDINARY_API_URL, formData, {
+                headers: {
+                    "Content-Type": "application/form-data",
+                },
+            });
             const newProduct = {
                 title: document.querySelector("#title").value,
-                image: document.querySelector("#image").value,
+                image: data.url,
                 content: document.querySelector("#content").value,
             };
             add(newProduct);
